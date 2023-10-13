014ee4811548c2b47df254edf4c781fb6d456921
Chris Golding <chris.golding@gmail.com>
2023-10-12 22:01:19 -0400

    Initial Commit

---
+++ WidgetTestv2/WidgetTestv2App.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetTestv2App.swift
+//  WidgetTestv2
+//
+//  Created by Chris Golding on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetTestv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
